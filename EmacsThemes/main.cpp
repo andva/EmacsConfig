8478273ba375fd4b7b406592bbcf7412ddbe2ae8
Andva <andreasvalter@gmail.com>
2013-02-20 18:04:11 +0100

    Tog bort filer som inte skulle vara med och fixade mera!

--- EmacsThemes/main.cpp
+++ EmacsThemes/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int main()
 {
-  int uma = 0;
-  int fav orit
+    int festarn = 0;
+    for(int i = 0; i < 10; i++)
+    {
+        if(i == festarn)
+        {
+            cout << "Hej Hej" << endl;
+            vector<int> v;
+            v.add(10);            
+        }
+    }
+
 }
--- _emacs.el
+++ _emacs.el
@@ -6,12 +6,37 @@
 ;; ErgoEmacs
 (setenv "ERGOEMACS_KEYBOARD_LAYOUT" "sv") ;
 (load-file "~/KeyBindings/ergoemacs_1.9.3.1/site-lisp/site-start.el")
+(xmsi-mode 0)
 
 ;; C++ mode
 (add-hook 'c++-mode-hook
   '(lambda ()
      (c-set-style "stroustrup")
      (setq indent-tabs-mode nil)))
+
+;; Add h to C++ mode
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+
+;; Use the GDB visual debugging mode
+(setq gdb-many-windows t)
+
+;; Turn Semantic on
+(semantic-mode 1)
+
+;; Try to make completions when not typing
+;; (global-semantic-idle-completions-mode 1)
+
+;; Use the Semantic speedbar additions
+(add-hook 'speedbar-load-hook (lambda () (require 'semantic/sb)))
+
+;; Treat .h files as C++ files (instead of C)
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+
+;; Return adds a newline and indents
+(define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
+
+;; Run compile when you press F5
+(global-set-key (kbd "<f5>") 'compile)
 
 ;; Move temp files to other dir
 (setq backup-directory-alist
@@ -33,10 +58,6 @@
 ;; Cursor type
 (setq-default cursor-type 'bar) 
 
-;; Turn of auto complete
-;; (auto-fill-mode -1)
-;; (flyspell-mode 0) 
-
 ;; Add color scheme
 (require 'color-theme)
 (require 'color-theme-sublime)
@@ -46,7 +67,11 @@
 ;; Set standard indent to 2 rather that 4
 (setq standard-indent 2)
 
-
+;; Scroll one line at a time
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; One line at a time
+(setq mouse-wheel-progressive-speed nil) ;; Don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+(setq scroll-step 1)  ;; keyboard scroll one line at a time
 
 
 
