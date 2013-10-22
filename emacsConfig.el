9d42576e339d0a2fa6f7ec1562a5ae37671fb64f
Valter <andreasvalter@gmail.com>
2013-10-22 14:16:24 +0200

    Added helm

--- emacsConfig.el
+++ emacsConfig.el
@@ -1,9 +1,15 @@
 ;; My config file!
-(defconst win32p    (eq system-type 'windows-nt)  
-  "Are we running on a Windows system?")
-(defconst linuxp    (or (eq system-type 'gnu/linux)  (eq system-type 'linux))  
-  "Are we running on Linux?")
 
+(setq load-path (cons (expand-file-name "~/EmacsConfig/modes") load-path))
+;; Line numbers
+(global-linum-mode t)
+;; Temp, startup
+(setq backup-directory-alist
+	  `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+	  `((".*" ,temporary-file-directory t)))
+
+;;;; Comments function
 (defun comment-or-uncomment-region-or-line ()
     "Comments or uncomments the region or the current line if there's no active region."
     (interactive)
@@ -14,7 +20,73 @@
         (comment-or-uncomment-region beg end)
         (next-line)))
 
-;; Latex
+;;;; Bindings
+;; Compile
+(global-set-key (kbd "C-`") 'compile)
+;; Toggle comment on region
+(global-set-key (kbd "C-/") 'comment-or-uncomment-region-or-line)
+;; Add comment at end of line
+(global-set-key (kbd "M-;") 'comment-dwim)
+;; Add new line and indent to right level
+(define-key global-map (kbd "RET") 'newline-and-indent)
+;; Remove with backspace
+(setq c-backspace-function 'backward-delete-char)
+
+;;;; Company mode
+(add-to-list 'load-path "~/EmacsConfig/modes/company-mode")
+(autoload 'company-mode "company" nil t)
+
+(defun complete-or-indent ()
+  (interactive)
+  (if (company-manual-begin)
+	  (company-complete-common)
+	(indent-according-to-mode)))
+
+(add-hook 'company-mode
+          (lambda()
+            (global-set-key (kbd "TAB") 'complete-or-indent)
+			(global-set-key "\t" 'complete-or-indent)))
+
+(setq company-backends '(company-elisp 
+                         company-ropemacs
+                         company-gtags
+                         company-dabbrev-code
+                         company-keywords
+                         company-files 
+                         company-dabbrev
+						 company-clang))
+(setq company-idle-delay t)
+
+(company-mode 1)
+
+;;;; Helm mode
+;; Responsible for autocomplete in M-X, helm-imenu is nice as wel.
+(add-to-list 'load-path "~/EmacsConfig/modes/helm")
+(require 'helm-config)
+(helm-mode t)
+
+;;;; Cpp and C
+;; Add h files to cpp mode
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+(defun my-c-mode-common-hook ()
+  ;; my customizations for all of c-mode, c++-mode, objc-mode, java-mode
+  (c-set-offset 'substatement-open 0)
+  ;; other customizations can go here
+  (hs-minor-mode t)
+  (setq c++-tab-always-indent t)
+  (setq c-basic-offset 4)                  ;; Default is 2
+  (setq c-indent-level 4)                  ;; Default is 2
+  (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60))
+  (setq tab-width 4)
+  (setq indent-tabs-mode t)  ; use spaces only if nil
+  )
+(add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
+
+;;;; Python
+(add-hook 'python-mode
+		  (setq compile-command "python "))
+
+;;;; TeX
 (setq TeX-auto-save t)
 (setq TeX-parse-self t)
 (setq-default TeX-master nil)
@@ -37,113 +109,61 @@
                 (TeX-view)
                 [return]))))
 
-(add-hook 'python-mode
-	  (setq compile-command "python "))
-
-(defun my-c-mode-common-hook ()
-  ;; my customizations for all of c-mode, c++-mode, objc-mode, java-mode
-  (c-set-offset 'substatement-open 0)
-  ;; other customizations can go here
-  (hs-minor-mode t)
-  (setq c++-tab-always-indent t)
-  (setq c-basic-offset 4)                  ;; Default is 2
-  (setq c-indent-level 4)                  ;; Default is 2
-  (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60))
-  (setq tab-width 4)
-  (setq indent-tabs-mode t)  ; use spaces only if nil
-  )
-(add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
-(setq-default tab-width 4)
-(setq standard-indent 4)
-;; Add h to C++ mode
-(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
-
-;; CMake
-(setq load-path (cons (expand-file-name "~/EmacsConfig/modes") load-path))
-(require 'cmake-mode)
-(setq auto-mode-alist
-      (append '(("CMakeLists\\.txt\\'" . cmake-mode)
-		("\\.cmake\\'" . cmake-mode))
-	      auto-mode-alist))
-
-;; GLSL
+;;;; GLSL
 (autoload 'glsl-mode "glsl-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.vert\\'" . glsl-mode))
 (add-to-list 'auto-mode-alist '("\\.frag\\'" . glsl-mode))
 
+;;;; CMake
+(setq load-path (cons (expand-file-name "~/EmacsConfig/modes") load-path))
+(require 'cmake-mode)
+(setq auto-mode-alist
+      (append '(("CMakeLists\\.txt\\'" . cmake-mode)
+				("\\.cmake\\'" . cmake-mode))
+			  auto-mode-alist))
+
+;;;; elisp
+(defun imenu-elisp-sections ()
+  (setq imenu-prev-index-position-function nil)
+  (add-to-list 'imenu-generic-expression '("Sections" "^;;;; \\(.+\\)$" 1) t))
+ 
+(add-hook 'emacs-lisp-mode-hook 'imenu-elisp-sections)
+
+;;;; Tab default
+(setq-default tab-width 4)
+(setq standard-indent 4)
+
+
+;;;; Debugging
 ;; Use the GDB visual debugging mode
 (setq gdb-many-windows t)
+;; Debug emacs config
+;; (setq c-echo-syntactic-information-p t)
 
-;; Line numbers
-(global-linum-mode t)
+;;;; Scrolling
+;; Scroll one line at a time
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; One line at a time
+(setq mouse-wheel-progressive-speed nil) ;; Don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+(setq scroll-step 1)  ;; keyboard scroll one line at a time
 
-;; Debug config
-(setq c-echo-syntactic-information-p t)
-
-(defun complete-or-indent ()
-    (interactive)
-    (if (company-manual-begin)
-        (company-complete-common)
-      (indent-according-to-mode)))
-
-;; Run compile when you press F5
-(global-set-key (kbd "C-`") 'compile)
-(global-set-key (kbd "C-/") 'comment-or-uncomment-region-or-line)
-(global-set-key (kbd "M-;") 'comment-dwim)
-(define-key global-map (kbd "RET") 'newline-and-indent)
-(setq c-backspace-function 'backward-delete-char)
-(add-hook 'company-mode
-          (lambda()
-            (global-set-key (kbd "TAB") 'complete-or-indent)))
-
-
-(setq load-path (cons (expand-file-name "~/EmacsConfig/modes") load-path))
-(add-to-list 'load-path "~/EmacsConfig/modes/company-mode")
-(autoload 'company-mode "company" nil t)
-(setq company-backends '(company-elisp 
-                         company-ropemacs
-                         company-gtags
-                         company-dabbrev-code
-                         company-keywords
-                         company-files 
-                         company-dabbrev))
-
-(company-mode t)
-
-(defun complete-or-indent ()
-    (interactive)
-    (if (company-manual-begin)
-        (company-complete-common)
-      (indent-according-to-mode)))
-
-(setq company-idle-delay t)
-;; Move temp files to other dir
-(setq backup-directory-alist
-`((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms
-`((".*" ,temporary-file-directory t)))
- 
-;; Remove startup screen
-(setq inhibit-startup-screen t)
-
+;;;; Font
 ;; Specify the default font as =Source Code Pro=, which should already
 ;;    be [[http://blogs.adobe.com/typblography/2012/09/source-code-pro.html][downloaded]] and installed.
-
 (set-frame-font "Source Code Pro")
 (set-face-attribute 'default nil :font "Source Code Pro" :height 80)
 (set-face-font 'default "Source Code Pro")
 
+
+;;;; Color theme
 (add-to-list 'load-path "~/EmacsConfig/color-theme/")
 (add-to-list 'load-path "~/EmacsConfig/color-theme/themes")
 (require 'color-theme)
 (require 'color-theme-molokai)
 (color-theme-molokai)
 
-;; Scroll one line at a time
-(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; One line at a time
-(setq mouse-wheel-progressive-speed nil) ;; Don't accelerate scrolling
-(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
-(setq scroll-step 1)  ;; keyboard scroll one line at a time
+;; Remove startup screen
+(setq inhibit-startup-screen t)
 
 ;; Match paranthesis
 (show-paren-mode 1)
